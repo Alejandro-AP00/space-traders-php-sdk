3a3fd34495ba6e5c954bfc6672a6438510f917f6
Blede <bledegraphics@gmail.com>
2025-05-06 21:37:05 -0400

    Cargo responses

---
+++ src/Responses/Fleet/Cargo/ExtractResourcesResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AlejandroAPorras\SpaceTraders\Responses\Fleet\Cargo;
+
+use AlejandroAPorras\SpaceTraders\Data\Fleet\ExtractionData;
+use AlejandroAPorras\SpaceTraders\Data\Fleet\ShipCargoData;
+use AlejandroAPorras\SpaceTraders\Data\CooldownData;
+use Saloon\Http\Response;
+
+class ExtractResourcesResponse extends Response
+{
+    public function extraction(): ExtractionData
+    {
+        return new ExtractionData($this->json('data.extraction'), $this->getConnector());
+    }
+
+    public function cargo(): ShipCargoData
+    {
+        return new ShipCargoData($this->json('data.cargo'), $this->getConnector());
+    }
+
+    public function cooldown(): CooldownData
+    {
+        return new CooldownData($this->json('data.cooldown'), $this->getConnector());
+    }
+}
